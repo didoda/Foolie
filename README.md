a7a2f8283e933c80fe222d1ccb9a3d1b7e8b088c
dante di domenico <dante@didoda.com>
2021-10-21 10:11:08 +0200

    feat: new version

--- README.md
+++ README.md
@@ -1,48 +1,10 @@
 # Foolie
-A dummy html page that reads rss feeds using Favella (text-to-speech javascript library)
 
-**Foolie** reads your rss feeds using **Favella** (https://github.com/batopa/favella) and comments them randomly (random comments and random 'voices').
-It's basically a standalone html page, with some javascript and css files.
+**Foolie** reads your rss feeds using **api.rss2json.com** and **Favella** (https://github.com/batopa/favella).
 
-## Download
-
-Download the [latest release](https://github.com/didoda/foolie/releases) in your localhost web folder (i.e.: /var/www/foolie).
-
-## Config
-
-Edit ```js/config.js``` to personalise your settings: rssUrl, lang, comments, maxComments, speakerSetup.
-```
-var config = {
-    rssUrl: 'http://www.newyorker.com/feed/news',
-    lang: 'en-GB',
-    comments: [
-        'Really?',
-        'No way',
-        'Oh my God!',
-        'Are you kidding me?',
-        'I can\'t believe it',
-        'Disgusting',
-        'Oh yeah',
-        'Remarkable',
-        'Are you serious or what?'
-    ],
-    maxComments: 3,
-    speakerSetup: { // favella setup
-        parentalControl: false,
-        speakOptions: {
-            volume: 1,
-            rate: 1,
-            pitch: 1,
-            lang: 'en-GB'
-        }
-    }
-};
-```
-
-## Enjoy
-
-Open with google chrome your Foolie (i.e. http://localhost/foolie).
-Edit again js/config.js to add funny comments or to change rss feed url or lang, etc.
+## Install
+If you clone this repo, launch `yarn && yarn dev` for development.
+`yarn build` will update `public` folder. You have to manually update `public/index.html` if you do a new build.
 
 ## Thanks to 
 
@@ -54,10 +16,10 @@ Favella is a cool javascript library that provides a text-to-speech service; it'
 Have fun with Favella!
 Thanks to Favella and to batopa.
 
-### jquery
-
-Foolie uses JQuery 3.5.1 (https://jquery.com/).
-
 ### rss2json
 
 Foolie uses rss2json api to obtain feed data (https://rss2json.com/).
+
+# Vue 3 + Vite
+
+Foolie is a component developed with [Vue 3](https://v3.vuejs.org/) in [Vite](https://vitejs.dev/).
---
+++ src/components/FoolieRssReader.vue
@@ -0,0 +1,134 @@
+<script setup>
+import Favella from 'favella';
+import axios from 'axios'
+
+defineProps({
+  url: String,
+  lang: {
+    type: String,
+    default: 'en-GB'
+  }
+})
+
+init();
+
+function init() {
+  Favella.setup({
+    parentalControl: false,
+    speakOptions: {
+      volume: 1,
+      rate: 1,
+      pitch: 1,
+      lang: 'en-GB',
+      mute: 'console'
+    }
+  });
+}
+
+async function readRss(url) {
+  const response = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(url));
+  if (response.data.feed && response.data.items) {
+    const config = {
+      parentalControl: false,
+      speakOptions: {
+        volume: 1,
+        rate: 1,
+        pitch: 1,
+        lang: document.getElementById('lang').value
+      }
+    };
+    Favella.setup(config);
+    for (let i = 0; i < response.data.items.length; i++) {
+      let feed = response.data.items[i];
+      await aloud(feed.title);
+      await aloud(feed.content);
+    }
+  }
+
+  return response;
+}
+
+async function aloud(content) {
+  await Favella.speak(content, {
+    onstart: function(e) {
+      document.getElementById('text').innerHTML = `${content}`;
+    }
+  });
+}
+
+async function play() {
+  if (Favella.isSpeaking()) {
+    return;
+  }
+  display('play', false);
+  display('pause', true);
+  display('resume', false);
+  display('stop', true);
+  const url = document.getElementById('rssUrl').value;
+  await readRss(url);
+}
+
+function pause() {
+  if (Favella.isPaused()) {
+    return;
+  }
+  Favella.pause();
+  display('play', false);
+  display('pause', false);
+  display('resume', true);
+  display('stop', true);
+}
+
+function resume() {
+  Favella.resume();
+  display('play', false);
+  display('pause', true);
+  display('resume', false);
+  display('stop', true);
+}
+
+function stop() {
+  if (!Favella.isSpeaking()) {
+    return;
+  }
+  Favella.cancel();
+  display('play', true);
+  display('pause', false);
+  display('resume', false);
+  display('stop', false);
+  document.getElementById('text').innerHTML = '';
+}
+
+function display(id, toggle) {
+  if (toggle) {
+    document.getElementById(id).classList.remove('hidden');
+  } else {
+    document.getElementById(id).classList.add('hidden');
+  }
+}
+</script>
+
+<template>
+
+  <div>
+
+    <input id="rssUrl" type="text" size="100" v-model="url" />
+
+    <select id="lang" v-model="lang">
+      <option value="en-GB">British English</option>
+      <option value="en-US">US English</option>
+      <option value="pt-BR">Portugues do Brazil</option>
+      <option value="es-ES">Espanol</option>
+      <option value="it-IT">Italiano</option>
+    </select>
+
+    <button id="play" type="button" @click="play">Play</button>
+    <button id="pause" class="hidden" type="button" @click="pause">Pause</button>
+    <button id="resume" class="hidden" type="button" @click="resume">Resume</button>
+    <button id="stop" class="hidden" type="button" @click="stop">Stop</button>
+
+    <div id="text"></div>
+
+  </div>
+
+</template>
